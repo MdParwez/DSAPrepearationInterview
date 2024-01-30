c36a04f4accd406710b7f51f0192cfeb5cb00b7f
Md Parwez <73132560+MdParwez@users.noreply.github.com>
2024-01-30 11:48:59 +0530

    Update finalDaySqlQ2.sql

--- finalDaySqlQ2.sql
+++ finalDaySqlQ2.sql
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/customers-who-bought-all-products/description/?envType=featured-list&envId=leetcode-curated-sql-70?envType=featured-list&envId=leetcode-curated-sql-70
 # Write your MySQL query statement below
 
-SELECT c.customer_id
-FROM Customer c
+SELECT customer_id
+FROM Customer 
 group by customer_id 
-HAVING count(distinct c.product_key) = (select count(distinct p.product_key) FROM Product p);
+HAVING count(distinct product_key) = (select count(distinct product_key) FROM Product );
